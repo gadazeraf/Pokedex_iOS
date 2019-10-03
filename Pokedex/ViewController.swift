9d23db10541a50b0e52c0df08fb89b85ad6c1f9b
Gad Azeraf <gad.azeraf@epita.fr>
2019-10-03 12:49:49 +0200

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Gad on 03/10/2019.
+//  Copyright © 2019 Azeraf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
